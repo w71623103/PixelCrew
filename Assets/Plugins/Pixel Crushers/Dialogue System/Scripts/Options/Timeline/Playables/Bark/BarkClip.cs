0e3952abd1df50ad27605d59eb508455c1690c71
Lyle Li <limit.1dr@gmail.com>
2022-08-22 14:50:39 +0800

    重新上传Dialogue System

---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Options/Timeline/Playables/Bark/BarkClip.cs
@@ -0,0 +1,35 @@
+// Recompile at 2022/8/22 14:45:23
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+// Copyright (c) Pixel Crushers. All rights reserved.
+
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [Serializable]
+    public class BarkClip : PlayableAsset, ITimelineClipAsset
+    {
+        public BarkBehaviour template = new BarkBehaviour();
+        public ExposedReference<Transform> listener;
+
+        public ClipCaps clipCaps
+        {
+            get { return ClipCaps.None; }
+        }
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<BarkBehaviour>.Create(graph, template);
+            BarkBehaviour clone = playable.GetBehaviour();
+            clone.listener = listener.Resolve(graph.GetResolver());
+            return playable;
+        }
+    }
+}
+#endif
+#endif
